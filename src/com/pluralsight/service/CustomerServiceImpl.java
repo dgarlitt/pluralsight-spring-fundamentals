40595e0e4464cb5a464a421d394da8eecf9671c9
Daniel Arlitt <daniel.arlitt@ticketmaster.com>
2015-01-28 20:34:29 -0800

    xml annotation configuration

--- src/com/pluralsight/service/CustomerServiceImpl.java
+++ src/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,16 +2,36 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by daniel.arlitt on 1/27/15.
  */
+
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-  private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+  @Autowired
+  private CustomerRepository customerRepository;
+
+  public CustomerServiceImpl() {}
+
+//  @Autowired
+  public CustomerServiceImpl(CustomerRepository customerRepository) {
+    System.out.println("Using constructor injection");
+
+    this.customerRepository = customerRepository;
+  }
+
+//  @Autowired
+  public void setCustomerRepository(CustomerRepository customerRepository) {
+    System.out.println("Using setter injection");
+
+    this.customerRepository = customerRepository;
+  }
 
   @Override
   public List<Customer> findAll() {
